cc1f732d89c4e022bf98f11822f179a11a314b11
Nuradiyana <me@nooradiana.com>
2023-11-08 10:24:41 +0700

    Tambah `SnakeCaseMapper` mapper name

--- src/DataTransfers/ProcurementDetailData.php
+++ src/DataTransfers/ProcurementDetailData.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace Inisiatif\Procurement\DataTransfers;
 
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Attributes\MapName;
+use Spatie\LaravelData\Mappers\SnakeCaseMapper;
 
+#[MapName(SnakeCaseMapper::class)]
 final class ProcurementDetailData extends Data
 {
     public function __construct(
